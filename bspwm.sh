8b6fbb5c7a7287d8bb658abc68e73e0f00d62be2
l1nux-th1ngz <137152321+l1nux-th1ngz@users.noreply.github.com>
2025-02-10 13:20:23 -0700

    Update bspwm.sh

--- bspwm.sh
+++ bspwm.sh
@@ -1,107 +1,48 @@
-#!/bin/bash
+sudo apt-get -y install bspwm polybar sxhkd alacritty brightnessctl dunst rofi jq 
+sudo apt-get -y install policykit-1-gnome git playerctl mpd ncmpcpp geany ranger 
+sudo apt-get -y install mpc picom xdotool feh ueberzug maim pamixer libwebp-dev
+sudo apt-get -y install xdg-user-dirs nala webp-pixbuf-loader zsh zsh-autosuggestions 
+sudo apt-get -y install zsh-syntax-highlighting nemo zip unzip kitty terminator
+sudo apt-get -y install gvfs gvfs-backends engrampa tint2 dmenu xdo jgmenu redshift xautolock 
+sudo apt-get -y install fzf ytfzf yt-dlp gawk tumbler gpick neofetch xdg-utils 
+sudo apt-get -y install python-is-python3 python3-gi gir1.2-nm-1.0 duf libglib2.0-bin btop
+sudo apt-get -y install ncdu bat exa wmctrl acpid xclip scrot acpi mpdris2 
+sudo apt-get -y install libplayerctl-dev gir1.2-playerctl-2.0 lxappearance bc
 
-# Main list of packages
-packages=(
-    "bspwm"
-    "sxhkd"
-    "polybar"
-    "suckless-tools"
-    "rxvt-unicode"
-    "firefox-esr"
-    "xorg"
-    "xserver-xorg"
-    "xbacklight"
-    "xbindkeys"
-    "xvkbd"
-    "xinput"
-    "build-essential"
-    "git"
-    "curl"
-    "wget"
-    "policykit-1-gnome"
-    "network-manager"
-    "network-manager-gnome"
-    "pamixer"
-    "nemo"
-    "nemo-archive-plugin"
-    "zip"
-    "dex"
-    "unzip"
-    "file-roller"
-    "lxappearance"
-    "dialog"
-    "mtools"
-    "dosfstools"
-    "avahi-daemon"
-    "acpi"
-    "acpid"
-    "gvfs"
-    "gvfs-backends"
-    "gnome-power-manager"
-    "pulseaudio"
-    "pavucontrol"
-    "pulsemixer"
-    "feh"
-    "fonts-recommended"
-    "fonts-font-awesome"
-    "fonts-terminus"
-    "papirus-icon-theme"
-    "exa"
-    "maim"
-    "rofi"
-    "scrot"
-    "slop"
-    "dunst"
-    "npm"
-    "libnotify-bin"
-    "xdotool"
-    "libnotify-dev"
-    "nnn"
-    "geany"
-    "geany-plugin-addons"
-    "geany-plugin-git-changebar"
-    "geany-plugin-spellcheck"
-    "geany-plugin-treebrowser"
-    "geany-plugin-markdown"
-    "geany-plugin-insertnum"
-    "geany-plugin-lineoperations"
-    "geany-plugin-automark"
-    "redshift"
-    "xdg-user-dirs-gtk"
-)
+git clone https://github.com/hidayry/dotfiles-bspwm.git
 
-# Function to install packages if they are not already installed
-install_packages() {
-    local pkgs=("$@")
-    local missing_pkgs=()
+cd dotfiles-bspwm
+# Make usable for everybody not just username ryan
+find . -type f -exec sed -i "s/ryan/$(whoami)/g" {} +
 
-    # Check if each package is installed
-    for pkg in "${pkgs[@]}"; do
-        if ! dpkg -l | grep -q " $pkg "; then
-            missing_pkgs+=("$pkg")
-        fi
-    done
+# Copy
+cp -r config/* ~/.config/
+cp -r local/* ~/.local/
+cp -r home/* ~/
 
-    # Install missing packages
-    if [ ${#missing_pkgs[@]} -gt 0 ]; then
-        echo "Installing missing packages: ${missing_pkgs[@]}"
-        sudo apt-get update
-        sudo apt-get -y install "${missing_pkgs[@]}"
-        if [ $? -ne 0 ]; then
-            echo "Failed to install some packages. Exiting."
-            exit 1
-        fi
-    else
-        echo "All required packages are already installed."
-    fi
-}
+echo "-- change this line =
+eww -c \"$HOME\"/.config/bspwm/$(whoami)/src/eww daemon &
+polybar -q $(whoami)-bar -c \"$HOME\"/.config/bspwm/$(whoami)/config.ini &
+#eww -c \"$HOME\"/.config/bspwm/$(whoami)/src/eww open bar &
 
-# Call function to install packages
-install_packages "${packages[@]}"
+-- become =
+#eww -c \"$HOME\"/.config/bspwm/$(whoami)/src/eww daemon &
+#polybar -q $(whoami)-bar -c \"$HOME\"/.config/bspwm/$(whoami)/config.ini &
+eww -c \"$HOME\"/.config/bspwm/$(whoami)/src/eww open bar &
 
-xdg-user-dirs-gtk-update
+-- in =
+~/.config/bspwm/bspwmrc"
 
-mkdir -p ~/.config/{bspwm,sxhkd,dunst}
+curl https://raw.githubusercontent.com/phenax/bsp-layout/master/install.sh | bash -
+wget https://github.com/erebe/greenclip/releases/download/v4.2/greenclip
+sudo mv greenclip /usr/bin/
+sudo chmod +x /usr/bin/greenclip
 
-install -Dm755 /usr/share/doc/bspwm/examples/bspwmrc ~/.config/bspwm/bspwmrc
-install -Dm644 /usr/share/doc/bspwm/examples/sxhkdrc ~/.config/sxhkd/sxhkdrc
+git clone https://github.com/noctuid/tdrop.git
+cd tdrop
+sudo make install
+cd ..
+
+git clone https://github.com/baskerville/xqp.git
+cd xqp
+sudo make install
